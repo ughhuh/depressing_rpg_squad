bc0c725194cad5c87d7d3c36385ae7d3b31ecc1f
Alena Galysheva <ab7723@student.jamk.fi>
2022-02-22 18:43:42 +0200

    sample scene update 1
    new: grid tiles, player, movement script, cinemachine setup.

---
+++ Assets/TestFolder/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 2f; //movement speed variable
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+        // get player input and store as floats
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+       
+        Vector2 inputVector = new Vector2(horizontalInput, verticalInput); // store player movement input as a vector
+        //inputVector = Vector2.ClampMagnitude(inputVector, 1); // preventing diagonal movement from being faster than horizontal/vertical movement
+
+        Vector2 playerMovement = inputVector * movementSpeed; // applying movement speed to player's input
+
+        rb.MovePosition(rb.position + playerMovement);
+
+    }
+
+}
